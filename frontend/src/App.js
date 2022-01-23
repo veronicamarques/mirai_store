66f201ce5f3147bbcb932b17ab231233d570049f
Vitor Luquini <vitorluquini2013@hotmail.com.br>
2022-01-23 18:38:43 -0300

    alteracão no header, mudanca da tipagem (typescript - javascript)

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Header from './components/navbar/index'
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <hr className="container" />
+    </>
+  );
+}
+
+export default App;
