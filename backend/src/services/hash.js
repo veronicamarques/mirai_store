6220eba339307aa986a3e5c960ef19a9730167ae
Vitor Luquini <vitorluquini2013@hotmail.com.br>
2022-02-27 19:21:17 -0300

    clean

--- backend/src/services/hash.js
+++
@@ -1,9 +0,0 @@
-import bcrypt from "bcryptjs";
-
-export const createPasswordHash = async (password) => {
-  return bcrypt.hash(password, 8);
-};
-
-export const checkPassword = async (password, hash) => {
-  return bcrypt.compare(password, hash);
-};
